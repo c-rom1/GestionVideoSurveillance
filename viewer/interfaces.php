b62ea70eaf2dcb0ab14c37a453ed09732d637456
c-rom1 <rcureau@gmail.com>
2018-05-31 23:05:19 +0200

    Ajout commentaires pour la redirection en fonction des appels Ajax

--- viewer/interfaces.php
+++ viewer/interfaces.php
@@ -9,13 +9,14 @@
 include("function/authentificationClass.php");
 include("function/functionClass.php");
 
-
+// appel ajax pour suppression de fichier
 if (isset($_POST['fichier'])) {
     $function = new functionClass();
     $function->supprFiles($_POST['fichier'], $_POST['type']);
     exit;
 }
 
+// dans le cas où l'on souhaite se déconnecter
 if (isset($_GET['logout'])) {
     if($_GET['logout'] == 1) {
         $auth = new authentificationClass();
